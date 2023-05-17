04d6881e07a7661bfbf6bf726f27ab174dba128b
shoko-nurs <shokonurs@gmail.com>
2023-05-17 19:30:01 +0600

    comments

--- task.go
+++ task.go
@@ -13,15 +13,16 @@ func main() {
 
 type data struct{}
 
-// Start function
+// Start function.
+// N is processor number
+// X is queue length. Channel max buffer
 func Start(N int, X int) {
 
 	runtime.GOMAXPROCS(N)
-
 	queue := make(chan data, X)
 
+	// Imitation of data input
 	var input string
-
 	fmt.Println("Type anything and press enter")
 
 	for {
@@ -42,11 +43,13 @@ func Start(N int, X int) {
 
 func process(ch chan data) {
 	/*
-		REAL CODE MUST BE:
+		настоящий код должен быть таким поидее:
 		 x := <-ch
 		*** do work with x for some time ***
 	*/
 
+	// Придется сначала ждать, и только потом читать из канала
+	// иначе буфер канала никогда не заполнится
 	time.Sleep(time.Second * 5)
 	<-ch
 	fmt.Println("COMPLETED")
